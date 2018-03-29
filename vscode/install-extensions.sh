676cd0716cc36463ebff4aa7ed91281a68745eab
Daniel Chang <daniel.chang85@gmail.com>
2018-03-29 11:26:07 -0400

    VScode extensions

---
+++ vscode/install-extensions.sh
@@ -0,0 +1,23 @@
+# code --list-extensions | xargs -L 1 echo code --install-extension
+
+code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension asvetliakov.snapshot-tools
+code --install-extension castwide.solargraph
+code --install-extension christian-kohler.path-intellisense
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension eamodio.gitlens
+code --install-extension enochc.copy-relative-path
+code --install-extension esbenp.prettier-vscode
+code --install-extension hedinne.popping-and-locking-vscode
+code --install-extension mauve.terraform
+code --install-extension ms-python.python
+code --install-extension ms-vscode.sublime-keybindings
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension naumovs.color-highlight
+code --install-extension oderwat.indent-rainbow
+code --install-extension rebornix.Ruby
+code --install-extension robertohuertasm.vscode-icons
+code --install-extension robinbentley.sass-indented
+code --install-extension wix.vscode-import-cost
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension ziyasal.vscode-open-in-github
