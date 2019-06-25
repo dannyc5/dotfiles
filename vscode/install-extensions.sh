7ab3f466a2c346bf033a0e2eab621a606a090da6
Daniel Chang <daniel.chang85@gmail.com>
2019-06-24 21:50:04 -0400

    Update vscode extensions and settings

--- vscode/install-extensions.sh
+++ vscode/install-extensions.sh
@@ -1,23 +1,28 @@
 # code --list-extensions | xargs -L 1 echo code --install-extension
 
-code --install-extension DavidAnson.vscode-markdownlint
 code --install-extension asvetliakov.snapshot-tools
 code --install-extension castwide.solargraph
 code --install-extension christian-kohler.path-intellisense
+code --install-extension DavidAnson.vscode-markdownlint
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension eamodio.gitlens
 code --install-extension esbenp.prettier-vscode
 code --install-extension ezforo.copy-relative-path-and-line-numbers
+code --install-extension GregorBiswanger.json2ts
 code --install-extension hedinne.popping-and-locking-vscode
 code --install-extension mauve.terraform
+code --install-extension mohsen1.prettify-json
 code --install-extension ms-python.python
 code --install-extension ms-vscode.sublime-keybindings
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
 code --install-extension msjsdiag.debugger-for-chrome
 code --install-extension naumovs.color-highlight
 code --install-extension oderwat.indent-rainbow
-code --install-extension rebornix.Ruby
-code --install-extension robertohuertasm.vscode-icons
+code --install-extension rebornix.ruby
 code --install-extension robinbentley.sass-indented
+code --install-extension stringham.move-ts
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension wesbos.theme-cobalt2
 code --install-extension wix.vscode-import-cost
 code --install-extension yzhang.markdown-all-in-one
 code --install-extension ziyasal.vscode-open-in-github
